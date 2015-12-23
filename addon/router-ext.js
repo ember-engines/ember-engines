24a289344342ffb4c0f5a2fc46edeaa68d196584
Robert Jackson <robert.w.jackson@me.com>
2015-12-23 15:33:56 -0500

    Adds ability to provide `resolveRouteMap` to RouterDSL.
    This requires changes in Ember made in
    https://github.com/emberjs/ember.js/pull/12749.

--- addon/router-ext.js
+++ addon/router-ext.js
@@ -1,7 +1,23 @@
 import Ember from 'ember';
 
 const {
-  Router
+  Router: EmberRouter,
+  RouterDSL: EmberRouterDSL,
+  get,
+  Logger,
+  getOwner
 } = Ember;
 
-Router.reopen();
+EmberRouter.reopen({
+  _buildDSL() {
+    let owner = getOwner(this);
+    let moduleBasedResolver = this._hasModuleBasedResolver();
+
+    return new EmberRouterDSL(null, {
+      enableLoadingSubstates: !!moduleBasedResolver,
+      resolveRouteMap(name) {
+        return owner._lookupFactory('route-map:' + name);
+      }
+    });
+  }
+});
