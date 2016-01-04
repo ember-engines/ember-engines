03137c81161731fbe7adfe32c6a1120e85654fec
Robert Jackson <robert.w.jackson@me.com>
2016-01-04 16:18:05 -0500

    Add basic test for `{{link-to}}`.

--- tests/acceptance/routeable-engine-demo-test.js
+++ tests/acceptance/routeable-engine-demo-test.js
@@ -3,12 +3,22 @@ import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | routeable engine demo');
 
-test('can invoke components from a routeable engine', function(assert) {
+test('can invoke components', function(assert) {
   visit('/routeable-engine-demo/blog/new');
 
   andThen(() => {
     assert.equal(currentURL(), '/routeable-engine-demo/blog/new');
 
     assert.equal(this.application.$('.routeable-hello-world').text().trim(), 'Hello, world!');
+  });
+});
+
+test('can render a link', function(assert) {
+  visit('/routeable-engine-demo/blog/post/1');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/routeable-engine-demo/blog/post/1');
+
+    assert.equal(this.application.$('a.routeable-post-comments-link').attr('href'), '/routeable-engine-demo/blog/post/1/comments');
   });
 });
