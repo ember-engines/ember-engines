5912e922ee74ae0f7cc3951feeec9cddad15507f
Dan Gebhardt <dan@cerebris.com>
2016-01-13 17:36:12 -0500

    Fix typo.

--- addon/initializers/engines.js
+++ addon/initializers/engines.js
@@ -8,7 +8,7 @@ import '../-private/keywords/outlet';
 import '../-private/router-dsl-ext';
 import '../-private/link-to-component-ext';
 
-// TODO: Move to ensure they are ran prior to instantiating Ember.Application
+// TODO: Move to ensure they run prior to instantiating Ember.Application
 export function initialize() {
 }
 
