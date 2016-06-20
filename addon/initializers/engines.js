ecbe8a641dadb57df13a543c14ec60ca90360d90
Dan Gebhardt <dan@cerebris.com>
2016-06-20 17:06:04 -0400

    [Clean] Remove overrides for router DSL pushed upstream.
    See https://github.com/emberjs/ember.js/pull/13733

--- addon/initializers/engines.js
+++ addon/initializers/engines.js
@@ -3,7 +3,6 @@ import '../-private/route-ext';
 import '../-private/engine-ext';
 import '../-private/engine-instance-ext';
 import '../-private/keywords/mount';
-import '../-private/router-dsl-ext';
 
 // TODO: Move to ensure they run prior to instantiating Ember.Application
 export function initialize() {
