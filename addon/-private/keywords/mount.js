df897ebe679edb3a6cfe8ee951fb9103979a0680
Dan Gebhardt <dan@cerebris.com>
2016-06-24 11:28:48 -0400

    Refactor `mount` keyword implementation.
    * Eliminates need to store engine directly on state. Instead, the 
      engine can be obtained via `getOwner(state.controller)`.
    
    * Introduces local convenience methods `lookupEngineController`,
      `lookupEngineView`, and `lookupEngineTemplate`.
    
    * Removes commented deadwood.

--- addon/-private/keywords/mount.js
+++ addon/-private/keywords/mount.js
@@ -1,65 +1,21 @@
 import Ember from "ember";
 import emberRequire from '../ext-require';
 
-// const internal = emberRequire('htmlbars-runtime').internal;
 const read = emberRequire('ember-htmlbars/streams/utils', 'read');
 const registerKeyword = emberRequire('ember-htmlbars/keywords', 'registerKeyword');
-// const legacyViewKeyword = emberRequire('ember-htmlbars/keywords/view');
 const ViewNodeManager = emberRequire('ember-htmlbars/node-managers/view-node-manager');
 const RenderEnv = emberRequire('ember-htmlbars/system/render-env');
 const EmptyObject = emberRequire('ember-metal/empty_object');
 
 const {
-  assert
+  assert,
+  getOwner
 } = Ember;
-
-// var isStable;
-// try {
-//   isStable = emberRequire('ember-htmlbars/keywords/real_outlet').default.isStable;
-// } catch (err) {
-//   isStable = emberRequire('ember-htmlbars/keywords/outlet').default.isStable;
-// }
-//
-// // Given an Ember Component, return the containing element
-// export function containingElement(view) {
-//   return view._renderNode.contextualElement;
-// }
-//
-// // This is Ember's {{#if}} predicate semantics (where empty lists
-// // count as false, etc).
-// export var shouldDisplay = emberRequire('ember-views/streams/should_display').default;
-//
-// // Finds the route name from a route state so we can apply our
-// // matching rules to it.
-// export function routeName(routeIdentity) {
-//   var o, r;
-//   if (routeIdentity && (o = routeIdentity.outletState) && (r = o.render)) {
-//     return [ r.name ];
-//   }
-// }
-//
-// // Finds the route's model from a route state so we can apply our
-// // matching rules to it.
-// export function routeModel(routeIdentity) {
-//   var o;
-//   if (routeIdentity && (o = routeIdentity.outletState)) {
-//     return [ o._lf_model ];
-//   }
-// }
-//
-// function withLockedModel(outletState) {
-//   var r, c;
-//   if (outletState && (r = outletState.render) && (c = r.controller) && !outletState._lf_model) {
-//     outletState = Ember.copy(outletState);
-//     outletState._lf_model = c.get('model');
-//   }
-//   return outletState;
-// }
 
 registerKeyword('mount', {
   willRender(renderNode, env) {
     if (env.view.ownerView._outlets) {
-      // We make sure we will get dirtied when outlet state changes.
+      // Make sure we will get dirtied when outlet state changes.
       env.view.ownerView._outlets.push(renderNode);
     }
   },
@@ -82,23 +38,20 @@ registerKeyword('mount', {
       env.owner.hasRegistration(`engine:${name}`)
     );
 
-    let engineInstance = buildEngineFromEnv(name, env);
+    let engineInstance = env.owner.buildChildEngineInstance(name);
 
-    let state = {
+    engineInstance.boot();
+
+    return {
       parentView: env.view,
       manager: prevState.manager,
-      controller: engineInstance.lookup('controller:application'),
-      childOutletState: childOutletState(name, env),
-      _engineInstance: engineInstance
+      controller: lookupEngineController(engineInstance),
+      childOutletState: childOutletState(name, env)
     };
-
-    return state;
   },
 
   childEnv(state, env) {
-    // let childEnv = env.childWithOutletState(state.childOutletState);
-    let childEnv = buildEnvForEngine(getEngineFromState(state), env);
-    return childEnv;
+    return buildEnvForEngine(getEngineFromState(state), env);
   },
 
   isStable(lastState, nextState) {
@@ -110,123 +63,23 @@ registerKeyword('mount', {
   },
 
   render(node, env, scope, params, hash, template, inverse, visitor) {
-    var state = node.getState();
-    // var name = params[0];
-    // var context = params[1];
+    let state = node.getState();
 
-    // var owner = env.owner;
+    let engineInstance = getEngineFromState(state);
 
-    // The render keyword presumes it can work without a router. This is really
-    // only to satisfy the test:
-    //
-    //     {{view}} should not override class bindings defined on a child view"
-    //
+    let engineController = lookupEngineController(engineInstance);
 
-    // let engineName = params[0];
+    let engineTemplate = lookupEngineTemplate(engineInstance);
 
-    let engineInstance = getEngineFromState(state); // owner.buildChildEngineInstance(engineName);
-    if (!engineInstance._booted) {
-      engineInstance.boot();
-    }
-
-    let engineController = engineInstance.lookup('controller:application');
-
-    // let engineView = engineInstance.lookup('view:application');
-
-    let engineTemplate = engineInstance.lookup('template:application');
-    //TODO - assert engineTemplate
-
-    // let router = owner.lookup('router:main');
-
-    // var templateName = 'template:' + name;
-    // assert(
-    //   'You used `{{render \'' + name + '\'}}`, but \'' + name + '\' can not be ' +
-    //   'found as either a template or a view.',
-    //   owner.hasRegistration('view:' + name) || owner.hasRegistration(templateName) || !!template
-    // );
-    //
-    var view = engineInstance.lookup('view:application');
-    if (!view) {
-      view = engineInstance.lookup('view:toplevel');
-    }
-    // var viewHasTemplateSpecified = view && !!get(view, 'template');
-    // if (!template && !viewHasTemplateSpecified) {
-    //   template = owner.lookup(templateName);
-    // }
-
-    if (view.ownerView !== env.view.ownerView) {
-      view.ownerView = env.view.ownerView;
-    }
-
-    // // provide controller override
-    // var controllerName;
-    // var controllerFullName;
-    //
-    // if (hash.controller) {
-    //   controllerName = hash.controller;
-    //   controllerFullName = 'controller:' + controllerName;
-    //   delete hash.controller;
-    //
-    //   assert(
-    //     'The controller name you supplied \'' + controllerName + '\' ' +
-    //     'did not resolve to a controller.',
-    //     owner.hasRegistration(controllerFullName)
-    //   );
-    // } else {
-    //   controllerName = name;
-    //   controllerFullName = 'controller:' + controllerName;
-    // }
-    //
-    // var parentController = read(scope.getLocal('controller'));
-    // var controller;
-    //
-    // // choose name
-    // if (params.length > 1) {
-    //   var factory = owner._lookupFactory(controllerFullName) ||
-    //                 generateControllerFactory(owner, controllerName);
-    //
-    //   controller = factory.create({
-    //     model: read(context),
-    //     parentController: parentController,
-    //     target: parentController
-    //   });
-    //
-    //   node.addDestruction(controller);
-    // } else {
-    //   controller = owner.lookup(controllerFullName) ||
-    //                generateController(owner, controllerName);
-    //
-    //   controller.setProperties({
-    //     target: parentController,
-    //     parentController: parentController
-    //   });
-    // }
-    //
-    // if (engineView) {
-    //   engineView.set('controller', engineController);
-    // }
-
-    // NOTE: setting state.controller now handled in `setupState`
-    // state.controller = engineController;
-
-    // hash.viewName = camelize(name);
-
-    if (engineTemplate && engineTemplate.raw) {
-      engineTemplate = engineTemplate.raw;
-    }
-
-    var options = {
+    let options = {
       layout: null,
       self: engineController
     };
 
-    // if (engineView) {
-    //   options.component = engineView;
-    // }
-
     let engineEnv = buildEnvForEngine(engineInstance, env);
 
-    var nodeManager = ViewNodeManager.create(node, engineEnv, hash, options, state.parentView, null, null, engineTemplate);
+    let nodeManager = ViewNodeManager.create(node, engineEnv, hash, options, state.parentView, null, null, engineTemplate);
+
     state.manager = nodeManager;
 
     nodeManager.render(engineEnv, hash, visitor);
@@ -242,13 +95,13 @@ registerKeyword('mount', {
 });
 
 function childOutletState(name, env) {
-  var topLevel = env.view.ownerView;
+  let topLevel = env.view.ownerView;
   if (!topLevel || !topLevel.outletState) { return; }
 
-  var outletState = topLevel.outletState;
+  let outletState = topLevel.outletState;
   if (!outletState.main) { return; }
 
-  var selectedOutletState = outletState.main.outlets['__ember_orphans__'];
+  let selectedOutletState = outletState.main.outlets['__ember_orphans__'];
   if (!selectedOutletState) { return; }
   var matched = selectedOutletState.outlets[name];
   if (matched) {
@@ -297,25 +150,39 @@ function isStableOutlet(a, b) {
 }
 
 function getEngineFromState(state) {
-  return state._engineInstance;
+  return getOwner(state.controller);
 }
 
-function buildEngineFromEnv(name, env) {
-  let engineInstance = env.owner.buildChildEngineInstance(name);
-  engineInstance.boot();
-  return engineInstance;
+function lookupEngineController(engineInstance) {
+  return engineInstance.lookup('controller:application');
+}
+
+function lookupEngineView(engineInstance, ownerView) {
+  let engineView = engineInstance.lookup('view:application') || engineInstance.lookup('view:toplevel');
+
+  if (engineView.ownerView !== ownerView) {
+    engineView.ownerView = ownerView;
+  }
+
+  return engineView;
+}
+
+function lookupEngineTemplate(engineInstance) {
+  let engineTemplate = engineInstance.lookup('template:application');
+
+  if (engineTemplate && engineTemplate.raw) {
+    engineTemplate = engineTemplate.raw;
+  }
+
+  return engineTemplate;
 }
 
 function buildEnvForEngine(engineInstance, parentEnv) {
-  let view = engineInstance.lookup('view:application') || engineInstance.lookup('view:toplevel');
-  let engineTemplate = engineInstance.lookup('template:application');
+  let engineView = lookupEngineView(engineInstance, parentEnv.view.ownerView);
 
-  if (view.ownerView !== parentEnv.view.ownerView) {
-    view.ownerView = parentEnv.view.ownerView;
-  }
+  let engineTemplate = lookupEngineTemplate(engineInstance);
 
-  let engineEnv = RenderEnv.build(view, engineTemplate.meta);
+  let engineEnv = RenderEnv.build(engineView, engineTemplate.meta);
 
   return engineEnv;
-
 }
