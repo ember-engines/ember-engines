a40cf0d910b5db3510108f941231e506f5125a09
Dan Gebhardt <dan@cerebris.com>
2016-07-14 23:06:52 -0400

    [Clean] `mount` keyword moved upstream.
    See https://github.com/emberjs/ember.js/pull/13820
    
    [Closes #155]

--- addon/initializers/engines.js
+++ addon/initializers/engines.js
@@ -2,7 +2,6 @@
 import '../-private/route-ext';
 import '../-private/engine-ext';
 import '../-private/engine-instance-ext';
-import '../-private/keywords/mount';
 
 // TODO: Move to ensure they run prior to instantiating Ember.Application
 export function initialize() {
