c2853c2832e2c19c50161de310f9d2ad68e0718a
Dan Gebhardt <dan@cerebris.com>
2016-07-22 11:26:42 -0400

    Engines feature flag enabled by default in Ember.
    See https://github.com/emberjs/ember.js/pull/13855

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -10,7 +10,6 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-        'ember-application-engines': true
       }
     },
 
