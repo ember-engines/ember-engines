14d9d07b96e86f1e33464645fc252cdd46804efa
Trent Willis <twillis@linkedin.com>
2016-08-18 11:09:00 -0700

    Add preloadAssets helper to fix tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,5 +2,8 @@ import resolver from './helpers/resolver';
 import {
   setResolver
 } from 'ember-qunit';
+import preloadAssets from './helpers/ember-engines/preload-assets';
+
+preloadAssets();
 
 setResolver(resolver);
