4eb7e61ff1f05c0891597c3571a4a5425f369afe
Trent Willis <trentmwillis@gmail.com>
2016-11-18 19:57:45 -0800

    Convert prepublish script to node script

---
+++ bin/install-test-addons.js
@@ -0,0 +1,13 @@
+var fs = require('fs-extra');
+
+fs.removeSync('node_modules/common-components');
+fs.symlinkSync('../tests/dummy/lib/common-components', 'node_modules/common-components');
+
+fs.removeSync('node_modules/eager-blog');
+fs.symlinkSync('../tests/dummy/lib/eager-blog', 'node_modules/eager-blog');
+
+fs.removeSync('node_modules/ember-blog');
+fs.symlinkSync('../tests/dummy/lib/ember-blog', 'node_modules/ember-blog');
+
+fs.removeSync('node_modules/ember-chat');
+fs.symlinkSync('../tests/dummy/lib/ember-chat', 'node_modules/ember-chat');
