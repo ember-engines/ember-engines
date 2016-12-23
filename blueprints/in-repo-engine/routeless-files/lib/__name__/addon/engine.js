ff2e7392aaed6e2e6004ea5634a200d835a000e4
Trent Willis <twillis@linkedin.com>
2016-12-22 18:46:20 -0800

    Add resolver.js file to in-repo-engine blueprint

--- blueprints/in-repo-engine/routeless-files/lib/__name__/addon/engine.js
+++ blueprints/in-repo-engine/routeless-files/lib/__name__/addon/engine.js
@@ -1,6 +1,6 @@
 import Engine from 'ember-engines/engine';
-import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
+import Resolver from './resolver';
 import config from './config/environment';
 
 const { modulePrefix } = config;
