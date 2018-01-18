1e0d54f05337c02e270789164075f6f47d2abb6b
Stefan Penner <stefan.penner@gmail.com>
2018-01-18 08:56:11 -0800

    test starting at ember-lts-2.16

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,10 +3,10 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
-      name: 'ember-lts-2.12',
+      name: 'ember-lts-2.16',
       npm: {
         devDependencies: {
-          'ember-source': '~2.12.0'
+          'ember-source': '~2.16.0'
         }
       }
     },
