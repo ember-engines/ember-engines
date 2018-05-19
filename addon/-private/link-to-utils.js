e65112fce11ac04be95c2f3fc26c9144ecea84e5
Michael Villander <nousvillander@gmail.com>
2018-05-19 15:39:56 -0300

    Fix Ember version import

--- addon/-private/link-to-utils.js
+++ addon/-private/link-to-utils.js
@@ -1,8 +1,7 @@
-import { VERSION } from '@ember/version'
-
+import Ember from 'ember';
 // LAME, but ¯\_(ツ)_/¯
 export default function hasEmberVersion(major, minor) {
-  var numbers = VERSION.split('-')[0].split('.');
+  var numbers = Ember.VERSION.split('-')[0].split('.');
   var actualMajor = parseInt(numbers[0], 10);
   var actualMinor = parseInt(numbers[1], 10);
   return actualMajor > major || (actualMajor === major && actualMinor >= minor);
