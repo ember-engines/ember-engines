040ea16b2e949fa1e579534e006cf0a4cb57ec57
Stefan Penner <stefan.penner@gmail.com>
2018-08-06 19:50:13 -0700

    remove ember-cli version test
    We don't support ember-cli versions this old anymore, and it breaks beta builds.

--- node-tests/unit/utils/should-compact-reexports-test.js
+++ node-tests/unit/utils/should-compact-reexports-test.js
@@ -37,7 +37,6 @@ describe('shouldCompactReexports', function() {
 
   it('returns true with recent versions of ember-cli-babel, ember-cli, and loader.js', function() {
     let addon = new FakeAddonWithDeps(fixture, {
-      'ember-cli': '2.13.0',
       'ember-cli-babel': '6.0.0',
       'loader.js': '4.4.0',
     });
@@ -47,17 +46,7 @@ describe('shouldCompactReexports', function() {
 
   it('returns false when the version of ember-cli-babel is < 6.0.0', function() {
     let addon = new FakeAddonWithDeps(fixture, {
-      'ember-cli': '2.13.0',
       'ember-cli-babel': '5.0.0',
-      'loader.js': '4.4.0',
-    });
-    expect(shouldCompactReexports(addon)).to.equal(false);
-  });
-
-  it('returns false when the version of ember-cli is < 2.13.0', function() {
-    let addon = new FakeAddonWithDeps(fixture, {
-      'ember-cli': '2.12.0',
-      'ember-cli-babel': '6.0.0',
       'loader.js': '4.4.0',
     });
     expect(shouldCompactReexports(addon)).to.equal(false);
@@ -65,7 +54,6 @@ describe('shouldCompactReexports', function() {
 
   it('returns false when the version of loader.js is < 4.4.0', function() {
     let addon = new FakeAddonWithDeps(fixture, {
-      'ember-cli': '2.13.0',
       'ember-cli-babel': '6.0.0',
       'loader.js': '4.3.0',
     });
