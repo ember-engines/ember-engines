f9149bd6ced1e211eeaef03c74039f02d3a194eb
Michael Villander <nousvillander@gmail.com>
2018-12-28 09:44:30 -0200

    fix routless engine demo test with new api

--- tests/acceptance/routeless-engine-demo-test.js
+++ tests/acceptance/routeless-engine-demo-test.js
@@ -1,38 +1,40 @@
-import { test } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { visit, click, find, currentURL } from '@ember/test-helpers';
+import $ from 'jquery';
 
-moduleForAcceptance('Acceptance | routeless engine demo');
+module('Acceptance | routeless engine demo', function (hooks) {
+  setupApplicationTest(hooks);
 
-test('can invoke components directly in an engine and also that are dependencies of an engine', async function(
-  assert
-) {
-  await visit('/routeless-engine-demo');
+  test('can invoke components directly in an engine and also that are dependencies of an engine', async function (
+    assert
+  ) {
+    await visit('/routeless-engine-demo');
 
-  assert.equal(currentURL(), '/routeless-engine-demo');
+    assert.equal(currentURL(), '/routeless-engine-demo');
 
-  assert.equal(
-    this.application
-    .$('.hello .greeting')
-    .text()
-    .trim(),
-    'Hello'
-  );
-  assert.equal(
-    this.application
-    .$('.hola .greeting')
-    .text()
-    .trim(),
-    'Hola'
-  );
-});
+    assert.equal(
+      $('.hello .greeting')
+        .text()
+        .trim(),
+      'Hello'
+    );
+    assert.equal(
+      $('.hola .greeting')
+        .text()
+        .trim(),
+      'Hola'
+    );
+  });
 
-test('can rerender a component in a routeless engine', async function(assert) {
-  await visit('/routeless-engine-demo');
+  test('can rerender a component in a routeless engine', async function (assert) {
+    await visit('/routeless-engine-demo');
 
-  let $clickCount = find('.click-count');
-  assert.equal($clickCount.text().trim(), '0');
+    let $clickCount = find('.click-count');
+    assert.equal($clickCount.textContent.trim(), '0');
 
-  await click('button.clicker');
+    await click('button.clicker');
 
-  assert.equal($clickCount.text().trim(), '1');
+    assert.equal($clickCount.text().trim(), '1');
+  });
 });
