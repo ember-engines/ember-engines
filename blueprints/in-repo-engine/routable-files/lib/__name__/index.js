57f20950a3ddae6d6b9b079f39404b8972ee1034
Michael Villander <nousvillander@gmail.com>
2019-04-12 10:56:49 -0300

    Use  on  config on blueprints

--- blueprints/in-repo-engine/routable-files/lib/__name__/index.js
+++ blueprints/in-repo-engine/routable-files/lib/__name__/index.js
@@ -6,9 +6,9 @@ const EngineAddon = require('ember-engines/lib/engine-addon');
 module.exports = EngineAddon.extend({
   name: '<%= dasherizedModuleName %>',
 
-  lazyLoading: {
+  lazyLoading: Object.freeze({
     enabled: <%= isLazy %>
-  },
+  }),
 
   isDevelopingAddon() {
     return true;
