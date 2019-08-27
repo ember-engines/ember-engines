efae9fb13f76240f8721705b5bd378638162f24b
Michael Villander <nousvillander@gmail.com>
2019-08-27 09:39:54 -0300

    adds transitionToRouteExternal into controller to engines

--- tests/dummy/lib/ember-blog/addon/controllers/post.js
+++ tests/dummy/lib/ember-blog/addon/controllers/post.js
@@ -2,4 +2,11 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   queryParams: ['lang'],
+  actions: {
+    goHomeProgrammatically() {
+      this.transitionToRouteExternal('home').then(() => {
+        this.set('transitionedToExternal', true);
+      });
+    }
+  }
 });
