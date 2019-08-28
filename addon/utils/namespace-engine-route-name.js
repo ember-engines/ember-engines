d3eabed29b0881e586a61f4cbadc6305056ff782
Michael Villander <nousvillander@gmail.com>
2019-08-27 21:44:04 -0300

    adds jsdoc in the util

--- addon/utils/namespace-engine-route-name.js
+++ addon/utils/namespace-engine-route-name.js
@@ -1,3 +1,12 @@
+/**
+ * This method is responsible for return route name from engine namespace
+ *
+ * @public
+ * @method namespaceEngineRouteName
+ * @param {String} mountPoint
+ * @param {String} routeName
+ * @return {String}
+ */
 export function namespaceEngineRouteName(mountPoint, routeName) {
   if (routeName === 'application') {
     return mountPoint;
