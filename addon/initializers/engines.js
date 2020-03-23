de31d5bd9b9f71ff03fcae3ee3bfb9cfc14ff9a9
Michael Villander <nousvillander@gmail.com>
2020-03-23 12:06:56 -0300

    Change import order

--- addon/initializers/engines.js
+++ addon/initializers/engines.js
@@ -1,9 +1,9 @@
 // Load extensions to Ember
 import '../-private/route-ext';
 import '../-private/router-ext';
-import '../-private/controller-ext';
 import '../-private/engine-ext';
 import '../-private/engine-instance-ext';
+import '../-private/controller-ext';
 
 // TODO: Move to ensure they run prior to instantiating Ember.Application
 export function initialize() {}
