f768db2f8e5e46fa93080f3d50e716915da4e5c0
Michael Villander <nousvillander@gmail.com>
2020-03-24 17:02:46 -0300

    update ember-cli to 3.17.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
+'use strict';
+
 module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
   },
   plugins: [
     'ember'
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,9 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('routeless-engine-demo');
@@ -32,5 +33,3 @@ Router.map(function() {
   // Used to check for collisions with Post route in ember-blog
   this.route('post', { path: '/post/:id' });
 });
-
-export default Router;
