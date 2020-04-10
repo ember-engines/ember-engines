b6b5a8f60522c2649e32c1389856f8c03a3445eb
Michael Villander <nousvillander@gmail.com>
2020-04-09 22:52:32 -0300

    Migrate post controller to octane

--- tests/dummy/lib/ember-blog/addon/controllers/post.js
+++ tests/dummy/lib/ember-blog/addon/controllers/post.js
@@ -1,39 +1,43 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { set, get } from '@ember/object';
+import { set, get, action } from '@ember/object';
 
-export default Controller.extend({
-  queryParams: ['lang'],
-  router: service(),
-  actions: {
-    transitionToHomeFromController() {
-      this.transitionToExternalRoute('home').then(() => {
-        set(this, 'transitionedToExternalRoute', true);
-      });
-    },
+export default class PostController extends Controller {
+  queryParams = ['lang'];
 
-    transitionToHomeByService() {
-      get(this, 'router').transitionToExternal('home').then(() => {
-        set(this, 'transitionToExternal', true);
-      });
-    },
+  @service router;
 
-    replaceWithHomeByService() {
-      get(this, 'router').replaceWithExternal('home').then(() => {
-        set(this, 'replaceWithExternal', true);
-      });
-    },
+  @action
+  transitionToHomeFromController() {
+    this.transitionToExternalRoute('home').then(() => {
+      set(this, 'transitionedToExternalRoute', true);
+    });
+  }
+  @action
+  transitionToHomeByService() {
+    get(this, 'router').transitionToExternal('home').then(() => {
+      set(this, 'transitionToExternal', true);
+    });
+  }
 
-    copyPostURL() {
-      const url = get(this, 'router').urlForExternal('home');
-      set(this, 'urlForExternal', url);
-      // Clipboard now has "/"
-    },
+  @action
+  replaceWithHomeByService() {
+    get(this, 'router').replaceWithExternal('home').then(() => {
+      set(this, 'replaceWithExternal', true);
+    });
+  }
 
-    checkActiveState() {
-      if (get(this, 'router').isActiveExternal('home')) {
-        set(this, 'isActiveExternal', true);
-      }
+  @action
+  copyPostURL() {
+    const url = get(this, 'router').urlForExternal('home');
+    set(this, 'urlForExternal', url);
+    // Clipboard now has "/"
+  }
+
+  @action
+  checkActiveState() {
+    if (get(this, 'router').isActiveExternal('home')) {
+      set(this, 'isActiveExternal', true);
     }
   }
-});
+}
