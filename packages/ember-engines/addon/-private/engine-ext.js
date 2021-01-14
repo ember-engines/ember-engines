f6d7f2cb38baa5a1bbb0f5f353d60c7936c87c2a
Robert Jackson <rjackson@linkedin.com>
2021-01-14 14:19:26 -0500

    Remove custom `<LinkTo />` override with Ember >= 3.24.1.

--- packages/ember-engines/addon/-private/engine-ext.js
+++ packages/ember-engines/addon/-private/engine-ext.js
@@ -1,14 +1,19 @@
 import Application from '@ember/application';
-import EngineScopedLinkComponent from '../components/link-to-component';
-import ExternalLinkComponent from '../components/link-to-external';
 import Engine from '@ember/engine';
+
+import { macroCondition, dependencySatisfies, importSync } from '@embroider/macros';
+
+import ExternalLinkComponent from '../components/link-to-external';
 
 Engine.reopen({
   buildRegistry() {
     let registry = this._super(...arguments);
 
     if (!(this instanceof Application)) {
-      registry.register('component:link-to', EngineScopedLinkComponent);
+      if (macroCondition(!dependencySatisfies('ember-source', '>= 3.24.1'))) {
+        const EngineScopedLinkComponent = importSync('../components/link-to-component').default;
+        registry.register('component:link-to', EngineScopedLinkComponent);
+      }
       registry.register('component:link-to-external', ExternalLinkComponent);
     }
 
