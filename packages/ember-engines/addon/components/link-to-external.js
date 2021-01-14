bdc186ed8f98aad85eb734c26ba5b3ec5dab246b
Robert Jackson <rjackson@linkedin.com>
2021-01-13 19:36:47 -0500

    Fix issues with LinkTo and LinkToExternal on Ember 3.24.0.
    Also includes a work around for 3.24.0 (which should be fixed in Ember
    3.24.1 once released).

--- packages/ember-engines/addon/components/link-to-external.js
+++ packages/ember-engines/addon/components/link-to-external.js
@@ -1,19 +1,35 @@
 import LinkComponent from '@ember/routing/link-component';
 import { getOwner } from '@ember/application';
 import { set, get } from '@ember/object';
+import { macroCondition, dependencySatisfies } from '@embroider/macros';
 
-export default LinkComponent.extend({
-  didReceiveAttrs() {
-    this._super(...arguments);
+let LinkToExternal;
 
-    const owner = getOwner(this);
+if (macroCondition(dependencySatisfies('ember-source', '> 3.24.0-alpha.1'))) {
+  LinkToExternal = class LinkToExternal extends LinkComponent {
+    _namespaceRoute(targetRouteName) {
+      const owner = getOwner(this);
+      const externalRoute = owner._getExternalRoute(targetRouteName);
 
-    if (owner.mountPoint) {
-      // https://emberjs.github.io/rfcs/0459-angle-bracket-built-in-components.html
-      const routeKey = 'targetRouteName' in this ? 'targetRouteName' : 'route';
-      const routeName = get(this, routeKey);
-      const externalRoute = owner._getExternalRoute(routeName);
-      set(this, routeKey, externalRoute);
+      return externalRoute;
     }
-  }
-});
+  };
+} else {
+  LinkToExternal = LinkComponent.extend({
+    didReceiveAttrs() {
+      this._super(...arguments);
+
+      const owner = getOwner(this);
+
+      if (owner.mountPoint) {
+        // https://emberjs.github.io/rfcs/0459-angle-bracket-built-in-components.html
+        const routeKey = 'targetRouteName' in this ? 'targetRouteName' : 'route';
+        const routeName = get(this, routeKey);
+        const externalRoute = owner._getExternalRoute(routeName);
+        set(this, routeKey, externalRoute);
+      }
+    }
+  });
+}
+
+export default LinkToExternal;
