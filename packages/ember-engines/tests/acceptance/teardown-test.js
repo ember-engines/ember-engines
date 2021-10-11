505cf7b431240347dc554290eece48989ce48812
Robert Jackson <rjackson@linkedin.com>
2021-10-11 10:53:07 -0400

    Run ember-qunit-codemod

--- packages/ember-engines/tests/acceptance/teardown-test.js
+++ packages/ember-engines/tests/acceptance/teardown-test.js
@@ -2,36 +2,36 @@ import { module, test } from 'qunit';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
 
-module('Acceptance | teardown test');
+module('Acceptance | teardown test', function() {
+  test('routeable engines clean up their container state', async function(assert) {
+    let service;
+    assert.expect(2);
+    this.application = startApp();
 
-test('routeable engines clean up their container state', async function(assert) {
-  let service;
-  assert.expect(2);
-  this.application = startApp();
+    await visit('/routable-engine-demo/blog/new');
 
-  await visit('/routable-engine-demo/blog/new');
+    assert.equal(currentURL(), '/routable-engine-demo/blog/new');
+    service = this.application.__container__.lookup('service:store')
+      .__exampleServiceForTesting;
 
-  assert.equal(currentURL(), '/routable-engine-demo/blog/new');
-  service = this.application.__container__.lookup('service:store')
-    .__exampleServiceForTesting;
+    destroyApp(this.application);
 
-  destroyApp(this.application);
+    assert.ok(service.isDestroyed, 'service got destroyed');
+  });
 
-  assert.ok(service.isDestroyed, 'service got destroyed');
-});
+  test('routeless engines clean up their container state', async function(assert) {
+    let service;
+    assert.expect(2);
+    this.application = startApp();
 
-test('routeless engines clean up their container state', async function(assert) {
-  let service;
-  assert.expect(2);
-  this.application = startApp();
+    await visit('/routeless-engine-demo');
 
-  await visit('/routeless-engine-demo');
+    assert.equal(currentURL(), '/routeless-engine-demo');
+    service = this.application.__container__.lookup('service:store')
+      .__exampleServiceForTesting;
 
-  assert.equal(currentURL(), '/routeless-engine-demo');
-  service = this.application.__container__.lookup('service:store')
-    .__exampleServiceForTesting;
+    destroyApp(this.application);
 
-  destroyApp(this.application);
-
-  assert.ok(service.isDestroyed, 'service got destroyed');
+    assert.ok(service.isDestroyed, 'service got destroyed');
+  });
 });
