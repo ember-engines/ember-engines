eec0095aec0ea021c01f2f7fa5b45af140a79e8d
villander <nousvillander@gmail.com>
2022-07-20 11:01:34 -0300

    Refactor the code and add js docs

--- packages/ember-engines/addon-test-support/index.js
+++ packages/ember-engines/addon-test-support/index.js
@@ -25,8 +25,32 @@ export function setupEngine(hooks, engineName) {
   });
 }
 
+/**
+ * Checks the owner to see if it has a registration for an Engine. This is a
+ * proxy to tell if an Engine's assets are loaded or not.
+ *
+ * @see https://github.com/ember-engines/ember-engines/blob/8f66b5e3b8089cd53884be49f270cac05f9a3d17/addon/-private/router-ext.js#L152-L164
+ *
+ * @param {Owner} owner
+ * @param {String} name
+ * @return {Boolean}
+ */
 function ownerHasEngine(owner, engineName) {
   return owner.hasRegistration(`engine:${engineName}`);
+}
+
+/**
+ * Registers an Engine that was recently loaded.
+ *
+ * @see https://github.com/ember-engines/ember-engines/blob/8f66b5e3b8089cd53884be49f270cac05f9a3d17/addon/-private/router-ext.js#L166-L182
+ *
+ * @param {Owner} owner
+ * @param {String} name
+ */
+function registerEngine(owner, engineName) {
+  // ownerHasEngine does registry lookup and caches "miss".
+  // With register() we tell registry that module is now available.
+  owner.register(`engine:${engineName}`, require(`${engineName}/engine`).default);
 }
 
 async function buildEngineOwner(owner, engineName) {
@@ -56,9 +80,7 @@ async function loadEngine(engineName) {
 
     await assetLoader.loadBundle(engineName);
 
-    // ownerHasEngine does registry lookup and caches "miss".
-    // With register() we tell registry that module is now available.
-    owner.register(`engine:${engineName}`, require(`${engineName}/engine`).default);
+    registerEngine(owner, engineName);
   }
 
   return buildEngineOwner(owner, engineName);
