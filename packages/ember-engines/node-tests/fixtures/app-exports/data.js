1752dfc9a85afbb658e0b429318aa43e811ac573
void_malex <void-mAlex@users.noreply.github.com>
2023-08-07 21:04:20 +0100

    add test for app re-export

---
+++ packages/ember-engines/node-tests/fixtures/app-exports/data.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  addon: {
+    components: {
+      'cant-see-me.js': `export default {};`
+    }
+  },
+  app: {
+    components: {
+      'cant-see-me.js': `export default {};`
+    }
+  }
+};
