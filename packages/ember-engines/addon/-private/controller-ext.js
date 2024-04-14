fcb282906ebd35460eb9f481ac03b98a4139b132
Sergey Astapov <sergeastapov@gmail.com>
2024-04-14 12:17:10 -0400

    [RFC 674] Deprecate transition methods of controller and route

--- packages/ember-engines/addon/-private/controller-ext.js
+++ packages/ember-engines/addon/-private/controller-ext.js
@@ -1,11 +1,14 @@
 import Controller from '@ember/controller';
 import { getOwner } from '@ember/application';
+import { deprecateTransitionMethods } from './deprecate-transition-methods';
 
 Controller.reopen({
   /*
     Creates an aliased form of a method that properly resolves external routes.
   */
   transitionToExternalRoute(routeName, ...args) {
+    deprecateTransitionMethods('controller', 'transitionToExternalRoute');
+
     let externalRoute = getOwner(this)._getExternalRoute(routeName);
     let target = this.target;
     let method = target.transitionToRoute || target.transitionTo;
