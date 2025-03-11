d071ace3b5d0efb9de52901d66f02aa6639ca437
Chris Manson <chris@manson.ie>
2025-03-11 18:12:57 +0000

    implement ember-cli-deprecation-workflow

---
+++ packages/ember-engines/tests/dummy/app/deprecation-workflow.js
@@ -0,0 +1,19 @@
+import setupDeprecationWorkflow from 'ember-cli-deprecation-workflow';
+
+setupDeprecationWorkflow({
+  workflow: [
+    {
+      handler: 'silence',
+      matchId: 'ember-engines.addon-test-support.engine-resolver-for',
+    },
+    { handler: 'silence', matchId: 'template-action' },
+    {
+      handler: 'silence',
+      matchId: 'ember-glimmer.link-to.positional-arguments',
+    },
+    {
+      handler: 'silence',
+      matchId: 'ember-engines.deprecation-router-service-from-host',
+    },
+  ],
+});
