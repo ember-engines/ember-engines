100246261d4f02248311a4ad2afe73f6781f4e1b
void_malex <void-mAlex@users.noreply.github.com>
2025-10-01 23:19:28 +0100

    remove redundant injection

--- packages/ember-blog-vite/addon/routes/post.js
+++ packages/ember-blog-vite/addon/routes/post.js
@@ -2,8 +2,6 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class PostRoute extends Route {
-  @service router;
-
   model(params) {
     return {
       user: this.modelFor('application'),
